469fb1251ece42f0e7c0e46c7641c47f87cdbd8e
Barak Eitan <barakeitan48@gmail.com>
2022-12-11 22:57:10 +0200

    add a function to send all products

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+const { response } = require("express");
 const express = require("express");
-const {default : mongoose} = require("mongoose");
+const { default: mongoose } = require("mongoose");
+const Product = require("./models/Product");
 
 const app = express();
 
+mongoose.connect("mongodb+srv://108user:108password@cluster0.hmdji17.mongodb.net/ReactChore2?retryWrites=true&w=majority", { useNewUrlParser: true })
+    .then(() => {
+        console.log("mongo connection open!!");
+    }).catch(err => {
+        console.log("no connection start");
+    });
+
 app.get('/', (req, res) => {
     res.status(200);
-})
+});
 
-app.listen(3000, () => {
-    console.log("app listening on port 3000");
-})
+app.get('/all', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        console.log({ products });
+        res.status(200).send({ products });
+    } catch (error) {
+        console.log(error)
+        res.send(error);
+    }
+});
+
+app.listen(3005, () => {
+    console.log("app listening on port 3005");
+});
